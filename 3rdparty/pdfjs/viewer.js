9f1fcd2d1a18e5ef8aa1fde24e99e4fc43dfd1bf
Lukas Reschke <lukas@statuscode.ch>
2014-06-17 23:05:28 +0200

    Decode instead of encode because we need to pass HTML values

--- 3rdparty/pdfjs/viewer.js
+++ 3rdparty/pdfjs/viewer.js
@@ -5145,10 +5145,10 @@ function webViewerLoad(evt) {
   // owncould customization to load file
   if (file && dir !== '') {
   	// Logged in
-  	PDFView.open(OC.linkTo('files', 'ajax/download.php')+"?files="+encodeURIComponent(window.file)+"&dir="+encodeURIComponent(window.dir), 1.0);
+  	PDFView.open(OC.linkTo('files', 'ajax/download.php')+"?files="+decodeURIComponent(window.file)+"&dir="+decodeURIComponent(window.dir), 1.0);
   } else {
   	// Public view
-  	PDFView.open(OC.linkTo('', 'public.php')+'?service=files'+"&t="+encodeURIComponent(window.file)+"&download", 1.0);
+  	PDFView.open(OC.linkTo('', 'public.php')+'?service=files'+"&t="+decodeURIComponent(window.file)+"&download", 1.0);
   }
 
 }
