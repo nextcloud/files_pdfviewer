cd31038d0698ea1c305590359b88a2b56e6d1666
Roeland Jago Douma <roeland@famdouma.nl>
2019-03-18 11:44:37 +0100

    Fix public page
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- js/previewplugin.js
+++ js/previewplugin.js
@@ -135,6 +135,9 @@ $(document).ready(function(){
 	if ($('#isPublic').val() && $('#mimetype').val() === 'application/pdf') {
 		$.urlParam = function(name){
 			var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+			if (results == null) {
+				return 0;
+			}
 			return results[1] || 0;
 		};
 		var sharingToken = $('#sharingToken').val();
