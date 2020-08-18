1e156160518540bcdac8468107c07320462e35f3
Roeland Jago Douma <roeland@famdouma.nl>
2020-08-18 13:05:29 +0200

    Add the framesrc csp by default
    Since we want to iframe the pdfviewer we need to properly set it. Else
    it might not work on some pages.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -27,12 +27,14 @@ declare(strict_types=1);
 
 namespace OCA\Files_PDFViewer\AppInfo;
 
+use OCA\Files_PDFViewer\Listeners\CSPListener;
 use OCA\Files_PDFViewer\Listeners\LoadViewerListener;
 use OCA\Viewer\Event\LoadViewer;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\Security\CSP\AddContentSecurityPolicyEvent;
 use OCP\Util;
 
 class Application extends App implements IBootstrap {
@@ -44,6 +46,7 @@ class Application extends App implements IBootstrap {
 
 	public function register(IRegistrationContext $context): void {
 		$context->registerEventListener(LoadViewer::class, LoadViewerListener::class);
+		$context->registerEventListener(AddContentSecurityPolicyEvent::class, CSPListener::class);
 	}
 
 	public function boot(IBootContext $context): void {
