nextcloud/files_pdfviewer

commit c992b55da9343e412f98e163a5066edcf6bdf37c

Fix opening PDF files with special characters in their name
Before the move to the viewer the URL of the PDF file to load was got
using "Files.getDownloadUrl()", which encodes each section of the path
as a URI component. The full URL, in turn, was again encoded as a URI
component to set the source of the iframe in which the PDF viewer is
loaded. When the PDF viewer parsed the URL it decoded it once, so each
section of the path was still encoded as a URI component.

After the move to the viewer the URL of the PDF file to load was got
from the "davPath" property set by the viewer, which uses the filename
as is. The full URL was then encoded as a URI component, so when the PDF
viewer parsed and decoded the URL the raw filename was used. In most
cases it worked fine, but if the filename included some special
character, like "?", the file failed to load.

Now, instead of using the "davPath" property set by the viewer, each
section of the dav path is encoded as a URI component. The encoding
tries to not make any assumption on the format used by the viewer to
avoid being coupled too much.

Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

src/views/PDFView.vue
@@ -33,8 +33,31 @@ export default {
 	computed: {
 		iframeSrc() {
 			return generateUrl('/apps/files_pdfviewer/?file={file}', {
-				file: this.davPath,
+				file: this.encodedDavPath,
 			})
+		},
+
+		encodedDavPath() {
+			const hasScheme = this.davPath.indexOf('://') !== -1
+
+			const pathSections = this.davPath.split('/')
+
+			// Ignore scheme and domain in the loop (note that the scheme
+			// delimiter, "//", creates an empty section when split by "/").
+			const initialSection = hasScheme ? 3 : 0
+
+			let encodedDavPath = ''
+			for (let i = initialSection; i < pathSections.length; i++) {
+				if (pathSections[i] !== '') {
+					encodedDavPath += '/' + encodeURIComponent(pathSections[i])
+				}
+			}
+
+			if (hasScheme) {
+				encodedDavPath = pathSections[0] + '//' + pathSections[2] + encodedDavPath
+			}
+
+			return encodedDavPath
 		},
 	},
 

commit 0b4e2dbe55710a93252f75a58ceba2d5d69f3967

Fix viewing PDF files with public share links
In nextcloud/files_pdfviewer#286, users have reported that PDF files
are not viewable via public share links. The issue was introduced in
c992b55 when attempting to encode parts of the URL for files that
have special characters.

This patch uses the URL Web API to deal with the parts of the URL
in a more specific way. Also, the path and files searchParams are
set based on `this.filename` and `this.basename` in the same way
it is done in the View module when setting `davPath`.

This has been tested on private and shared public links on files
with and without special characters.

src/views/PDFView.vue
@@ -38,26 +38,18 @@ export default {
 		},
 
 		encodedDavPath() {
-			const hasScheme = this.davPath.indexOf('://') !== -1
+			const hasScheme = ~this.davPath.indexOf('://')
+			const url = new URL(hasScheme ? this.davPath : 'https://.' + this.davPath)
+			const pathname = url.pathname
 
-			const pathSections = this.davPath.split('/')
+			url.pathname = pathname
 
-			// Ignore scheme and domain in the loop (note that the scheme
-			// delimiter, "//", creates an empty section when split by "/").
-			const initialSection = hasScheme ? 3 : 0
+			url.searchParams.set('path', this.filename.replace(this.basename, ''))
+			url.searchParams.set('files', this.basename)
 
-			let encodedDavPath = ''
-			for (let i = initialSection; i < pathSections.length; i++) {
-				if (pathSections[i] !== '') {
-					encodedDavPath += '/' + encodeURIComponent(pathSections[i])
-				}
-			}
+			const encodedDavPath = url.toString()
 
-			if (hasScheme) {
-				encodedDavPath = pathSections[0] + '//' + pathSections[2] + encodedDavPath
-			}
-
-			return encodedDavPath
+			return hasScheme ? encodedDavPath : encodedDavPath.split('https://.')[1]
 		},
 	},
 