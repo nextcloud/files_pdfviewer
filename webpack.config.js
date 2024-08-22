70200e3dcefdeae8c9267b7a79174dba827a6d76
Ferdinand Thiessen <opensource@fthiessen.de>
2024-08-22 18:38:14 +0200

    fix: Use the user configured UI language for PDFjs instead of browser language
    PDFjs uses the browser language by default, but users should have consistently the Nextcloud defined language.
    So now we set the PDFjs language to:
    1. The Nextcloud configured language if supported
    2. The unflavored version if supported (e.g. `de` instead of `de-DE`)
    3. Fall back to browser UI language
    
    We need 3 because if we set an unsupported lanuage PDFjs will default to English,
    which is worse than the browser UI language.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+const webpack = require('webpack')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 const path = require('path')
+const { readdirSync } = require('fs')
+
+const l10nContent = readdirSync(path.resolve(__dirname, 'js', 'pdfjs', 'web', 'locale'))
 
 webpackConfig.entry.workersrc = path.resolve(path.join('src', 'workersrc.js'))
 webpackConfig.entry.public = path.resolve(path.join('src', 'public.js'))
 
 // keep pdfjs vendor in the js folder
 webpackConfig.output.clean = false
+
+// Add list of PDFjs supported languages
+webpackConfig.plugins.push(
+	new webpack.DefinePlugin({
+		SUPPORTED_LANGUAGES: JSON.stringify(l10nContent),
+	}),
+)
 
 module.exports = webpackConfig
