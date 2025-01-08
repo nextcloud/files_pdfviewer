44bffe8a3ff8a8c3c977380a6b8bba93bef1242e
Daniel Calviño Sánchez <danxuliu@gmail.com>
2025-01-08 17:54:15 +0100

    Move PDFViewerApplication setup from helper script to Vue component
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/workersrc.js
+++ src/workersrc.js
@@ -4,57 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import logger from './services/logger.js'
 import redirectIfNotIframe from './utils/redirectIfNotIframe.js'
 
 // Checks if the page is displayed in an iframe. If not redirect to /.
 redirectIfNotIframe()
-
-// Retrieve the hideDownload from the url, this is
-// the most easy way to pass the prop to this iframe
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-const hideDownload = urlParams.get('hideDownload')
-
-function initializeCustomPDFViewerApplication() {
-	if (hideDownload === '1') {
-		const pdfViewer = window.document.querySelector('.pdfViewer')
-
-		if (pdfViewer) {
-			pdfViewer.classList.add('disabledTextSelection')
-		}
-
-		if (PDFViewerApplication) {
-			// Disable download function when downloads are hidden, as even if the
-			// buttons in the UI are hidden the download could still be triggered
-			// with Ctrl|Meta+S.
-			PDFViewerApplication.download = function() {
-			}
-
-			// Disable printing service when downloads are hidden, as even if the
-			// buttons in the UI are hidden the printing could still be triggered
-			// with Ctrl|Meta+P.
-			// Abuse the "supportsPrinting" parameter, which signals that the
-			// browser does not fully support printing, to make PDFViewer disable
-			// the printing service.
-			// "supportsPrinting" is a getter function, so it needs to be deleted
-			// before replacing it with a simple value.
-			delete PDFViewerApplication.supportsPrinting
-			PDFViewerApplication.supportsPrinting = false
-
-			// When printing is not supported a warning is shown by the default
-			// "beforePrint" function when trying to print. That function needs to
-			// be replaced with an empty one to prevent that warning to be shown.
-			PDFViewerApplication.beforePrint = function() {
-			}
-		}
-
-		logger.info('Download, print and user interaction disabled')
-	} else {
-		logger.info('Download and print available')
-	}
-
-	logger.debug('Initialized files_pdfviewer', PDFViewerApplicationOptions.getAll())
-}
-
-document.addEventListener('DOMContentLoaded', initializeCustomPDFViewerApplication, true)
