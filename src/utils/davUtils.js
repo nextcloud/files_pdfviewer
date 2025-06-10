26737003559fd038d4c2efabf6ca58c8364b2825
Daniel Calviño Sánchez <danxuliu@gmail.com>
2025-06-10 14:32:31 +0200

    fix: Fix eslint perfectionist/sort-imports
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- src/utils/davUtils.js
+++ src/utils/davUtils.js
@@ -2,8 +2,8 @@
  * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import { generateRemoteUrl } from '@nextcloud/router'
 import { getCurrentUser } from '@nextcloud/auth'
+import { generateRemoteUrl } from '@nextcloud/router'
 
 /**
  * Get the current dav root path
