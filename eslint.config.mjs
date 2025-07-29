2b7fc09b82dc699ba00cc65ea4282e291e79e388
Daniel Calviño Sánchez <danxuliu@gmail.com>
2025-07-29 20:17:16 +0200

    chore: Bump @nextcloud/eslint-config from 8.4.2 to 9.0.0-rc.5
    Bumping the version with:
    npm install @nextcloud/eslint-config@9.0.0-rc.5
    failed with:
    Conflicting peer dependency: eslint@9.34.0
    
    Due to that the version was bumped with:
    npm install --save-dev eslint@9.34.0
    npm install @nextcloud/eslint-config@9.0.0-rc.5
    npm uninstall eslint
    
    Besides that the configuration file was migrated from the old
    ".eslintrc.js" to the new "eslint.config.mjs".
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { recommendedVue2 } from '@nextcloud/eslint-config'
+
+export default [
+	...recommendedVue2,
+]
